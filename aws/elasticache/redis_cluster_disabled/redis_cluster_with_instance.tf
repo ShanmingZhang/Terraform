a9c12f4e9062dee07fc22f816da10a4beedf5856
Shanming Zhang <shanming.zhang@outlook.com>
2019-12-27 18:03:27 +0900

    delte

--- aws/elasticache/redis_cluster_disabled/redis_cluster_with_instance.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_elasticache_cluster" "instance_replica_cluster" {
-  cluster_id           = "cluster-example"
-  replication_group_id = aws_elasticache_replication_group.iac_redis_cluster_disabled.id
-}
